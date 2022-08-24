1c3e647f16dbc663c541af10701c1a282efa5e45
Brenda Liu <brenda.liu24@gmail.com>
2022-08-24 12:52:54 -0500

    add test for sql

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,12 +1,20 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/* Helper Function for updating company and user models
+* Converts object keys into SQL column name format
+* Input: {key:value} => {setCols: ... values: ...}
+*
+* Example:
+* {firstName: 'Aliya', age: 32} => {setCols:"first_name"=$1', '"age"=$2',
+                                    values: ["Aliya", 32]
+* 
+*/
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
---
+++ helpers/sql.test.js
@@ -0,0 +1,29 @@
+
+const { sqlForPartialUpdate } = require("./sql");
+
+
+
+describe("sql partial update", function () {
+  test("works: converts obj to sql", function () {
+    
+    const {setCols, values} = sqlForPartialUpdate({firstName: 'Aliya', age: 32},{
+      firstName: "first_name",
+      age: "age",
+    })
+    
+    expect(setCols).toEqual("\"first_name\"=$1, \"age\"=$2")
+    expect(values).toEqual(["Aliya", 32])
+  });
+
+  test("throw error on invalid input", function () {   
+    try{
+      const resp = sqlForPartialUpdate({},{
+        firstName: "first_name",
+        age: "age",
+      })
+
+    } catch (err){
+      expect(err.message).toBe("No data")
+    }
+  });
+})
